fd1a9f90390aa05cb9b7b64d1dff773a2924a2e1
James Li <lijamez@users.noreply.github.com>
2018-02-27 00:07:21 -0800

    Adding explicit @JsonCreator constructor for Genre.

--- src/main/java/net/tonbot/plugin/tmdb/Genre.java
+++ src/main/java/net/tonbot/plugin/tmdb/Genre.java
@@ -1,10 +1,21 @@
 package net.tonbot.plugin.tmdb;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Data;
 
 @Data
 class Genre {
-
+	
 	private final int id;
 	private final String name;
+	
+	@JsonCreator
+	public Genre(
+			@JsonProperty("id") int id, 
+			@JsonProperty("name") String name) {
+		this.id = id;
+		this.name = name;
+	}
 }
